56023c9c6a1f40d3485eab828399722d2db15b06
Vasil Lazarov <vaskosound@gmail.com>
2013-05-13 22:12:37 +0300

    change enum MineType to static class. Create method GetExplodeType

--- Battle-Field-4/Mine.cs
+++ Battle-Field-4/Mine.cs
@@ -20,6 +20,28 @@ namespace BattleField
             }
         }
 
+        private int[,] GetExplodeType(int[,] field, int x, int y)
+        {
+            int[,] explodeType = new int[5, 5];
+            switch (field[x, y])
+            {
+                case 1: explodeType = MineType.minePowerOne;
+                    break;
+                case 2: explodeType = MineType.minePowerTwo;
+                    break;
+                case 3: explodeType = MineType.minePowerThree;
+                    break;
+                case 4: explodeType = MineType.minePowerFour;
+                    break;
+                case 5: explodeType = MineType.minePowerFive;
+                    break;
+                default:
+                    throw new ArgumentException("Invalid mine type!");
+            }
+
+            return explodeType;
+        }
+
         public int Explode()
         {
             throw new System.NotImplementedException();
--- Battle-Field-4/MineType.cs
+++ Battle-Field-4/MineType.cs
@@ -5,13 +5,34 @@ using System.Text;
 
 namespace BattleField
 {
-    public enum MineType
+    public static class MineType
     {
-        minePowerOne = 1,
-        minePowerTwo = 2,
-        minePowerThree = 3,
-        minePowerFour = 4,
-        minePowerFive = 5
+        public static readonly int[,] minePowerOne = {{0,0,0,0,0},
+                                              {0,1,0,1,0},
+                                              {0,0,1,0,0},
+                                              {0,1,0,1,0},
+                                              {0,0,0,0,0}};
+
+        public static readonly int[,] minePowerTwo = {{0,0,0,0,0},
+                                                {0,1,1,1,0},
+                                                {0,1,1,1,0},
+                                                {0,1,1,1,0},
+                                                {0,0,0,0,0}};
+        public static readonly int[,] minePowerThree = {{0,0,1,0,0},
+                                                {0,1,1,1,0},
+                                                {1,1,1,1,1},
+                                                {0,1,1,1,0},
+                                                {0,0,1,0,0}};
+        public static readonly int[,] minePowerFour = {{0,1,1,1,0},
+                                                {1,1,1,1,1},
+                                                {1,1,1,1,1},
+                                                {1,1,1,1,1},
+                                                {0,1,1,1,0}};
+        public static readonly int[,] minePowerFive = {{1,1,1,1,1},
+                                                {1,1,1,1,1},
+                                                {1,1,1,1,1},
+                                                {1,1,1,1,1},
+                                                {1,1,1,1,1}};
     }
 }
 
