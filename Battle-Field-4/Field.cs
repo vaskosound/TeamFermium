25839c012ebc330f042e32a55f702c91d0733070
Valeri Colov <valeri.colov@gmail.com>
2013-05-15 08:35:43 +0300

    Method FillTheField from the class Field

--- Battle-Field-4/Field.cs
+++ Battle-Field-4/Field.cs
@@ -8,21 +8,56 @@ namespace BattleField
     public class Field
     {
         private int dimension;
+        private int[,] matrixForField;
 
         public int Dimension
         {
             get
             {
-                throw new System.NotImplementedException();
+                return this.dimension;
             }
             set
             {
+                //Do we need a check?
+                this.dimension = value;
             }
         }
 
-        public void GenerateFillForTheField()
+        public int[,] MatrixForField
         {
-            throw new System.NotImplementedException();
+            get { return this.matrixForField; }
+            set { this.matrixForField = value; }
+        }
+
+        public Field(int dimension)
+        {
+            this.Dimension = dimension;
+        }
+
+        public void FillTheField()
+        {
+            Random random = new Random(); //vhoid i inicializaciq na n i matricata;
+
+            int dimension = this.Dimension;
+            int[,] arr = this.MatrixForField;
+            int minPercentOfMines = 15 * dimension * dimension / 100;
+            int maxPercentOfMines = 30 * dimension * dimension / 100;
+
+            int mineNumber = random.Next(minPercentOfMines, maxPercentOfMines + 1); 
+
+            for (int i = 0; i < mineNumber; i++)
+            {
+                int row = random.Next(0, dimension);
+                int col = random.Next(0, dimension);
+
+                while (arr[row, col] != 0)
+                {
+                    row = random.Next(0, dimension);
+                    col = random.Next(0, dimension);
+                }
+
+                arr[row, col] = random.Next(1, 6);
+            }
         }
 
         public void Print()
