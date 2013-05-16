03110528bd7670a24df46df669ee330f1540a483
Vasil Lazarov <vaskosound@gmail.com>
2013-05-16 21:57:33 +0300

    Add Unit Test to class Mine. Move classes to new project Mines

---
+++ MinesTest/TestMine.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BattleField;
+
+namespace Mines.Test
+{
+    [TestClass]
+    public class TestMine
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestCreateMineWithIHigherType()
+        {
+            Mine mine = new Mine(6);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestCreateMineWithILowerType()
+        {
+            Mine mine = new Mine(0);
+        }
+
+        [TestMethod]
+        public void TestCreateMineWithICorrectType()
+        {
+            Mine mine = new Mine(4);
+            Assert.AreEqual(4, mine.Type);
+        }
+
+        [TestMethod]
+        public void TestMethodExplodeTypeWithInvalidData()
+        {
+            Mine mine = new Mine(4);
+            int[,] expected = {{0,1,1,1,0},
+                            {0,1,1,1,1},
+                            {1,1,1,1,1},
+                            {1,1,1,1,1},
+                            {0,1,1,1,0}};
+            int[,] actual = mine.ExplodeType();
+            CollectionAssert.AreNotEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMethodExplodeTypeWithCorrectData()
+        {
+            Mine mine = new Mine(4);
+            int[,] expected = {{0,1,1,1,0},
+                            {1,1,1,1,1},
+                            {1,1,1,1,1},
+                            {1,1,1,1,1},
+                            {0,1,1,1,0}};
+            int[,] actual = mine.ExplodeType();
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+    }
+}
